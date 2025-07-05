7feb49dca00286ae1c1b05b5cc7535bdd876c5fb
ldthien203 <96572606+ldthien203@users.noreply.github.com>
2025-07-05 21:13:12 +0700

    delete duplicate js file

--- src/pages/Home/components/ChartSection/ChartSection.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ChartSection = () => {
-    return (<section>
-      <h1>chart section</h1>
-    </section>);
-};
-exports.default = ChartSection;
